d2fed38dea8b383bca33d1990a687200c2143fdb
Mireziz-Memmedov <azzmmmdv@gmail.com>
2026-05-09 16:19:36 +0000

    setting.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -160,8 +160,10 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587            
 EMAIL_USE_TLS = True   
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+# EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = 'astroping2025@gmail.com'
+EMAIL_HOST_PASSWORD = 'drda nbuj dxyd pidw'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
