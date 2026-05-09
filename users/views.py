9fa15be82cb42849a2bf33c604a2316da0629291
Mireziz-Memmedov <azzmmmdv@gmail.com>
2026-05-09 17:08:31 +0000

    views.py

--- users/views.py
+++ users/views.py
@@ -12,7 +12,6 @@ from django.conf import settings
 import secrets
 import string
 import os
-import threading
 
 @api_view(["POST"])
 def signup(request):
@@ -70,24 +69,28 @@ def signup(request):
     user.set_password(password)
     user.save()
 
-    def send_email():
-        try:
-            send_mail(
-                subject='Email Verification Code',
-                message=f'Your verification code is: {verify_code}',
-                from_email=settings.DEFAULT_FROM_EMAIL,
-                recipient_list=[email],
-                fail_silently=False,
-            )
-        except Exception as e:
-            print("EMAIL ERROR:", e)
+    try:
 
-    threading.Thread(target=send_email).start()
+        send_mail(
+            subject = 'Email Verification Code',
+            message=f'Your verification code is: {verify_code}',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[email],
+            fail_silently=False,
+        )
 
-    return Response(
-        {'success': True, 'message': 'Verification code sent successfully'}, 
-        status=status.HTTP_201_CREATED
-    )
+        return Response({
+            'success': True, 
+            'message': 'Verification code sent successfully'
+        })
+
+    except Exception as e:
+        print("EMAIL ERROR:", str(e))
+        
+        return Response({
+            "success": False,
+            "error": str(e)
+        }, status=500)    
 
 def generate_verify_code(length):
     characters = string.digits
