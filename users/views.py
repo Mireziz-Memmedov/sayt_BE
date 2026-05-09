6750529694745e19d1762f58199b08080d01fc5e
Mireziz-Memmedov <azzmmmdv@gmail.com>
2026-05-09 17:15:55 +0000

    views.py

--- users/views.py
+++ users/views.py
@@ -70,11 +70,10 @@ def signup(request):
     user.save()
 
     try:
-
         send_mail(
-            subject = 'Email Verification Code',
+            subject='Email Verification Code',
             message=f'Your verification code is: {verify_code}',
-            from_email=settings.DEFAULT_FROM_EMAIL,
+            from_email=settings.EMAIL_HOST_USER,
             recipient_list=[email],
             fail_silently=False,
         )
